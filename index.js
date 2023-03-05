2d23aceb4926c462c4f939ab11bf9e2488d43c0d
Dorin Porcereanu <porcereanu.dorin@gmail.com>
2023-03-05 14:26:37 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(John, Birthday) {
+    let thankYouMessages = [];
+    for (let i = 0; i < John.length; i++) {
+      thankYouMessages.push(`Thank you, ${John[i]}, for the wonderful ${Birthday} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
